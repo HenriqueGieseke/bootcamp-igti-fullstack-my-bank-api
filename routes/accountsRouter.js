4bb9fb3e8d3e9664b53ad63a6c95f0fd5fcb351a
HenriqueGieseke <70168143+HenriqueGieseke@users.noreply.github.com>
2021-03-21 18:40:04 -0300

    melhoria do comentário no endpoint prime

--- routes/accountsRouter.js
+++ routes/accountsRouter.js
@@ -215,7 +215,7 @@ app.get('/higherBalance/:listSize', async (req, res) => {
   }
 });
 
-//clientes prime - um cliente com maior saldo de cada agência
+//clientes prime - um cliente com maior saldo de cada agência adicionado para agência 99
 app.patch('/prime', async (req, res) => {
   try {
     const accounts = await accountModel.find({});
